ff941efce8d5cc179db252497e80c56124dabcac
drstrangegithub <72202965+drstrangegithub@users.noreply.github.com>
2021-09-15 18:48:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-const express = require('express')
-const app = express();
-const port = 3000
-
-app.get('/', (req, res) => res.send('Hey there!'))
-
-app.listen(port, () =>
-console.log(`Your app is listening a https://localhost/${port}`)
-);
-
 /**
  * Module Imports
  */
