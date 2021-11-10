8329f440b41715724cd3fabd1d8d0f8c24c60942
DrStrange_Github <72202965+drstrangegithub@users.noreply.github.com>
2021-11-10 14:39:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ i18n.configure({
  * Client Events
  */
 client.on("ready", () => {
-  console.log(`${client.user.username} ready!`);
+  console.log(`${client.user.username} is ready!!`);
   client.user.setActivity(`${PREFIX}help`, { type: "LISTENING" });
 });
 client.on("warn", (info) => console.log(info));
